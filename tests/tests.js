7267234a85fe41d85a2d691a782b1ed24c45073b
willemlarsen@gmail.com <willem@alchemycodelab.com>
2019-04-05 14:37:22 -0700

    [with qunit window] greetFullName returns a greeting

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,16 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function greetFullName(first, last) {
+    return "Hello " + first + " " + last + "!";
+}
+
+test('greetFullName returns a greeting', function (assert) {
+    // Arrange
+    const expected = "Hello Willem Larsen!";
+    const firstName = "Willem";
+    const lastName = "Larsen";
+    // Act 
+    const result = greetFullName(firstName, lastName);
+    // Assert
+    assert.equal(result, expected);
 });
