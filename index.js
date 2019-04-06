0780faaaa6578097da78dea638b790027dd2cf50
willemlarsen@gmail.com <willem@alchemycodelab.com>
2019-04-06 12:01:11 -0700

    wire new feature into page logic

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import greetFullName from './src/greetFullName.js';
 
 const firstNameInput = document.getElementById('first-name');
+const middleNameInput = document.getElementById('middle-name');
 const lastNameInput = document.getElementById('last-name');
 const greeter = document.getElementById('greeter');
 const button = document.getElementById('greet-me');
 
 function greetVisitor() {   
     const firstName = firstNameInput.value;
+    const middleName = middleNameInput.value;
     const lastName = lastNameInput.value;
     
-    const greeting = greetFullName(firstName, lastName);
+    const greeting = greetFullName(firstName, middleName, lastName);
 
     greeter.textContent = greeting;
 }
